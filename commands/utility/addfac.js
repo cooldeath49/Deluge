d4e03e49862789131f573f22d951bf5ea03cf3c0
Arthur You <arthuritisyou@gmail.com>
2024-10-20 01:09:31 -0400

    modified add fac and password

--- commands/utility/addfac.js
+++ commands/utility/addfac.js
@@ -59,39 +59,8 @@ module.exports = {
         
         console.log(hex + " " + letter + letternumber + "k" + grid);
         let fac = addFacility(hex, letter, letternumber, grid, reg);
-        interaction.followUp({content: "Would you like to set a password?", components: [row]});
 
-        const collectorFilter = i => i.user.id === interaction.user.id;
-
-        try {
-          const confirmation = await interaction.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-
-          if (confirmation.customId == 'add pw') {
-            const modal = new ModalBuilder()
-            .setCustomId("pw modal")
-            .setTitle("Deluge");
-
-            const textBuilder = new TextInputBuilder()
-            .setCustomId("pw text")
-            .setLabel("Please enter a password.")
-            .setStyle(TextInputStyle.Short);
-
-            const actionRow = new ActionRowBuilder().addComponents(textBuilder);
-
-            modal.addComponents(actionRow);
-            await confirmation.showModal(modal);
-            
-            // confirmation.update({content: "Got a yes!", components: []});
-          } else if (confirmation.customId == 'reject edit fac') {
-            confirmation.update({content: "Got a no!", components: []});
-          }
-
-        } catch (e) {
-          console.log(e);
-          await interaction.followUp({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-        }
-
-        
+  
       } else {
         interaction.editReply("Could not find target hex \"" + hex + "\"");
       }
