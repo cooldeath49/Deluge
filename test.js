0791f9a470f96967b3bcd14bea0fbe8fdc2fcab6
Arthur You <arthuritisyou@gmail.com>
2025-01-20 10:38:06 -0500

    hmmmmmmmmmmm

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/*fetch("https://war-service-live.foxholeservices.com/api/worldconquest/maps/DeadLandsHex/dynamic/public")
+fetch("https://war-service-live.foxholeservices.com/api/worldconquest/maps/DeadLandsHex/dynamic/public")
     .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -10,39 +10,44 @@
   })
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
-  });*/
-
-  const sharp = require("sharp");
+  });
 
 
-  async function ah() {
-    await sharp("./HexImages/MapAcrithiaHex.png")
-    .rotate(45)
-    .grayscale()
-    .toFormat("jpeg", {mozjpeg: true})
-    .toFile("acrithia.jpeg");
+  const path = require("path");
+const sharp = require("sharp");
+
+
+// Combine the base image and the text image
+
+// Define the base image path
+const baseImagePath = path.resolve('marker1.png');
+
+// Create the text image
+
+async function ah() {
+  const meta = await sharp("marker1.png").metadata();
+  console.log(meta);
+  const mapText = await sharp({
+    text: {
+      text: "12",
+      align: 'center',
+      rgba: true,
+      font: '50px',
+      // fontfile: ITALIC_FONT
+    },
+  }).png();
+  const meta2 = await mapText.metadata();
+  const buffer = await mapText.toBuffer();
+  console.log(meta2);
+  try {
+    await sharp(baseImagePath)
+    .composite([{ input: buffer, top: 25, left: parseInt(meta.width/2 - meta2.width/2) }])
+    .toFile('output-image.png');
+    console.log("we did it")
+  } catch (er) {
+    console.log(er)
   }
+    
+}
 ah();
 
-let a = "hi";
-let c;
-switch (a) {
-
-  case "ha": {
-    let b = 4;
-    console.log(b);
-    break;
-  }
-  case "hi": {
-    let b = 5;
-    c = b;
-    console.log(c);
-    break;
-  }
-  case "no": {
-    let b = 6;
-    console.log(b);
-    break;
-  }
-}
-console.log(c);
